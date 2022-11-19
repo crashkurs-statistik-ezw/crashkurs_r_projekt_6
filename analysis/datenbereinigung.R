0d15148a46091a887027c4dc97bc38cdca2b2223
Christian <burkhart.christian@gmail.com>
2022-11-19 13:03:11 +0100

    remove code

--- analysis/datenbereinigung.R
+++ analysis/datenbereinigung.R
@@ -2,61 +2,42 @@
 
 # 6.0.1 Pakete laden
 # Lade das Paket janitor, skimr und haven
-library(janitor)
-library(skimr)
-library(haven)
 
 
 # 6.0.2 Daten einlesen
 # Lese den Datensatz data/replication_data_ramirez_beilock.csv ein
 # und speichere ihn in der Variable replication_data
-replication_data <- read_csv("data/replication_data_ramirez_beilock.csv")
 
 
 # 6.1 Datenbereinigung ----------------------------------------------------
 
 # 6.1.1 Anzahl Spalten und Reihen
 # Zeige dir die Anzahl der Spalten und Reihen im Datensatz an
-ncol(replication_data)
-nrow(replication_data)
 
 
 # 6.1.2 Relevante Variablen ansehen
 # * Führe folgenden Code aus und untersuche die für uns relevanten Daten.
 # * Beschreibe die Variablen ID, STAI, Pretest.High und Posttest.High anhand
 #   der angezeigten Histogramme
-replication_data %>% 
-  select(ID, STAI, Pressure, Condition, 
-         Pretest.High, Posttest.High) %>% 
-  skim()
 
 
 # 6.1.3 Variablen selektieren und Variablennamen reinigen
 # * Selektiere die für uns relevanten Variablen mit select
 # * Reinige die Variablennamen mit Hilfe der Funktion clean_names
 # * Speichere den Datensatz in der Variable replication_data_subset
-replication_data_subset <- replication_data %>% 
-  select(ID, STAI, Pressure, Condition, 
-         Pretest.High, Posttest.High) %>%
-  clean_names()
 
 
 # 6.1.4 Proband*innen zählen
 # * Zähle wie viele Reihen es pro Proband*in gibt. 
 # * Schaue dir die Daten der beiden Personen an, indem du den Datensatz
 # um diese beiden Personen filterst
-count(replication_data_subset, id)
-
-replication_data_subset %>% 
-  filter(id %in% c(1, 2))
 
 
 # 6.1.5 Proband*innen entfernen
 # * Entferne die Probanden, welche bei stai, pretest_high und
 #   posttest_high 0-er Werte haben
 # * Speichere den Datensatz in der Variable replication_data_subset_unique
-replication_data_subset_unique <- replication_data_subset %>% 
-  filter(!((id %in% c(1, 2)) & pretest_high == 0 & posttest_high == 0))
+# TODO: Ressource filter mit Operatoren
 
 
 # 6.2 Daten transformieren ------------------------------------------------
@@ -68,31 +49,15 @@ replication_data_subset_unique <- replication_data_subset %>%
 # Deine Aufgabe ist es, die Daten so zu transformieren, dass der Datensatz
 # diese Struktur hat. Verwende hierfür pivot_longer
 # Speichere den Datensatz in der Variable replication_data_long
-replication_data_long <- replication_data_subset_unique %>% 
-  pivot_longer(
-    cols = contains("high"),
-    names_to = "test",
-    values_to = "accuracy"
-  )
 
 
 # 6.2.2 Test umschreiben
 # Kodiere die Variable test folgendermaßen um:
 # pretest_high -> Pretest
 # posttest_high -> Posttest
-replication_data_long <- replication_data_long %>% 
-  mutate(
-    test = case_when(
-      test == "pretest_high" ~ "Pretest",
-      test == "posttest_high" ~ "Posttest",
-      TRUE ~ test
-    )
-  )
 
 # 6.3 Daten exportieren -----------------------------------------------------
 
 # 6.3.1 csv und sav speichern
 # Speichere den Datensatzreplication_data_long  als CSV und SAV-Datei
 # unter data/export mit dem Dateinamen replication_cleaned.csv
-write_csv(replication_data_long, "data/export/replication_cleaned.csv")
-write_sav(replication_data_long, "data/export/replication_cleaned.sav")
--- analysis/datenexploration.R
+++ analysis/datenexploration.R
@@ -3,13 +3,11 @@
 
 # 6.4.1 Skimr laden
 # Lade das Paket skimr
-library(skimr)
 
 
 # 6.4.2 Datensatz laden
 # * Lade den Datensatz data/export/replication_cleaned.csv
 # * Speichere den Datensatz in der Variable buttrick_data
-buttrick_data <- read_csv("data/export/replication_cleaned.csv")
 
 
 # 6.4.3 Faktoren releveln
@@ -17,10 +15,6 @@ buttrick_data <- read_csv("data/export/replication_cleaned.csv")
 #   speichere den Leveln folgende Reihenfolge: 
 #   Pretest -> Posttest
 # * Speichere den Datensatz in der Variable buttrick_data
-buttrick_data <- buttrick_data %>% 
-  mutate(
-    test = as.factor(test) %>% fct_relevel("Pretest", "Posttest")
-  )
 
 
 # 6.5 Deskriptive Statistik -----------------------------------------------
@@ -29,29 +23,6 @@ buttrick_data <- buttrick_data %>%
 # * Berechne die Mittelwerte und Standardabweichungen der Variable
 #   accuracy für die beiden Gruppen und die Testzeitpunkte
 # * Verwende mindestens zwei Methoden, um zu den Ergebnissen zu kommen
-buttrick_data %>% 
-  select(accuracy, condition, pressure, test) %>% 
-  group_by(condition, test, pressure) %>% 
-  skim()
-
-buttrick_data %>% 
-  select(accuracy, condition, pressure, test) %>% 
-  group_by(condition, test, pressure) %>% 
-  summarise(
-    mean_accuracy = mean(accuracy),
-    sd_accuracy = sd(accuracy)
-  )
-
-buttrick_data %>% 
-  select(accuracy, condition, pressure, test) %>% 
-  group_by(condition, test, pressure) %>% 
-  summarise(
-    across(
-      .cols = accuracy,
-      .fns  = list(mean = ~ mean(.x, na.rm = TRUE),
-                   sd  =  ~ sd(.x, na.rm = TRUE))
-    )
-  )
 
 
 # 6.6 Datenvisualisierung --------------------------------------------------
@@ -64,14 +35,7 @@ buttrick_data %>%
 # * Stelle auf der Y-Achse den Posttest dar
 # * Verwende facet_wrap um die Gruppen in pressure voneinander zu 
 #   unterscheiden
-buttrick_data %>% 
-  pivot_wider(
-    names_from = "test",
-    values_from = "accuracy"
-  ) %>% 
-  ggplot(aes(x = Pretest, y = Posttest)) +
-  geom_point() +
-  facet_wrap(vars(pressure))
+
 
 
 # 6.6.2 Histogramme darstellen
@@ -79,19 +43,7 @@ buttrick_data %>%
 # * In den Spalten soll die Variable test und in den 
 #   Reihen die Variable condition dargestellt sein
 # * Vervollständige den Code
-buttrick_data %>% 
-  ggplot(aes(x = accuracy)) +
-  geom_histogram(alpha = .7, color = "gray20",
-                 binwidth = 1) +
-  scale_y_continuous(expand = expansion(c(0, 0.3))) +
-  facet_grid(cols = vars(test), rows = vars(condition)) +
-  labs(
-    x = "Akkuratheit",
-    y = "Häufigkeit"
-  ) +
-  theme(
-    legend.position = "bottom"
-  )
+
 
 
 # 6.6.3 Boxplots darstellen
@@ -101,18 +53,7 @@ buttrick_data %>%
 # * Die Experimentallgruppen (condition) sollten unterschiedliche Füllfarben
 #   haben und als Boxplots dargestellt werden
 # * Vervollständige den Code
-buttrick_data %>% 
-  ggplot(aes(x = test, y = accuracy)) +
-  geom_boxplot(aes(fill = condition), alpha = .8) + 
-  scale_y_continuous(limits = c(0, 25)) +
-  facet_wrap(vars(pressure)) +
-  labs(
-    x = "Messzeitpunkt",
-    y = "Akkuratheit"
-  ) +
-  theme(
-    legend.position = "bottom"
-  )
+
 
 
 # 6.6.4 Mittelwerte als Liniendiagramm visualisieren
@@ -123,27 +64,3 @@ buttrick_data %>%
 #   und als Linien dargestellt werden
 # * Die Variable pressure sollte durch ein facet_wrap unterteilt werden
 # * Vervollständige den Code
-buttrick_data %>% 
-  select(accuracy, condition, test, pressure) %>% 
-  group_by(condition, test, pressure) %>% 
-  summarise(
-    across(
-      .cols = accuracy,
-      .fns  = list(mean = ~ mean(.x, na.rm = TRUE),
-                   sd  =  ~ sd(.x, na.rm = TRUE))
-    )
-  ) %>% 
-  ungroup() %>% 
-  ggplot(aes(x = test, y = accuracy_mean, color = condition,
-             group = condition)) + 
-  geom_point(size = 3) +
-  geom_line() +
-  labs(
-    x = "Test",
-    y = "Akkuratheit",
-    color = "Experimentalgruppe"
-  ) +
-  facet_wrap(vars(pressure)) +
-  theme(
-    legend.position = "bottom"
-  )
